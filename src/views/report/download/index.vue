c8ac8284c28bb563aa74d4ce8ad39fa7838bc0f0
李威 <470074654@qq.com>
2024-05-11 16:28:28 +0800

    添加报表页面

---
+++ src/views/report/download/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab-container">
+    <p>敬请期待~</p>
+    <el-form :model="queryForm" :inline="true">
+      <el-form-item label="公寓" prop="apartmentId">
+        <el-select placeholder="请选择" v-model="queryForm.apartmentId">
+          <el-option v-for="item in apartmentOption" default-first-option="true" :key="item.key" :label="item.value" :value="item.key"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="月份" prop="month">
+        <el-date-picker v-model="queryForm.month" type="month" value-format="yyyy-MM" placeholder="选择月">
+        </el-date-picker>
+      </el-form-item>
+      <el-button type="primary">下载报表</el-button>
+      <el-button type="primary">发送报表</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+import store from '@/store'
+
+export default {
+  name: 'Report',
+  data() {
+    return {
+      queryForm: {},
+      apartmentOption: store.getters.apartments
+    }
+  },
+  created() {
+    this.queryForm.apartmentId = this.apartmentOption[0].key
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    margin: 20px;
+  }
+</style>
---
+++ src/views/report/template/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tab-container">
+    <el-upload
+      class="upload-demo"
+      action="http://localhost:8080/rent/uploadFile"
+      :before-remove="beforeRemove"
+      multiple
+      :file-list="fileList">
+      <el-button size="small" type="primary">点击上传</el-button>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Template',
+  data() {
+    return {
+      fileList: []
+    }
+  },
+  methods: {
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    margin: 20px;
+  }
+</style>
